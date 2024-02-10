8d4a103c400aa694a882133947f6c4af8d59c558
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-02-10 09:52:48 -0300

    uolhost-challenge: Add initial commit.

---
+++ src/main/java/github/com/rosivaldolucas/uolhostchallenge/UolhostChallengeApplication.java
@@ -0,0 +1,13 @@
+package github.com.rosivaldolucas.uolhostchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolhostChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolhostChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/rosivaldolucas/uolhostchallenge/UolhostChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.rosivaldolucas.uolhostchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolhostChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
