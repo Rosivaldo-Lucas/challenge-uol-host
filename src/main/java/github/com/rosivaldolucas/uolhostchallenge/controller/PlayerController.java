6d318a3df6ec474c826b9d3e85eab87f590bc925
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-01-25 09:19:26 -0300

    add integration with Thymelef

--- src/main/java/github/com/rosivaldolucas/uolhostchallenge/controller/PlayerController.java
+++ src/main/java/github/com/rosivaldolucas/uolhostchallenge/controller/PlayerController.java
@@ -3,13 +3,15 @@ package github.com.rosivaldolucas.uolhostchallenge.controller;
 import github.com.rosivaldolucas.uolhostchallenge.dto.CreatePlayerDTO;
 import github.com.rosivaldolucas.uolhostchallenge.dto.PlayerResponseDTO;
 import github.com.rosivaldolucas.uolhostchallenge.service.PlayerService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/players")
 public class PlayerController {
 
@@ -20,15 +22,19 @@ public class PlayerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PlayerResponseDTO>> list() {
+    public ModelAndView list() {
         List<PlayerResponseDTO> playerList = this.playerService.list();
-        return ResponseEntity.status(HttpStatus.OK).body(playerList);
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("players", playerList);
+
+        return modelAndView;
     }
 
     @PostMapping
-    public ResponseEntity<PlayerResponseDTO> create(@RequestBody CreatePlayerDTO createPlayerDTO) {
-        PlayerResponseDTO playerResponseDTO = this.playerService.create(createPlayerDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(playerResponseDTO);
+    public String create(CreatePlayerDTO createPlayerDTO) {
+        this.playerService.create(createPlayerDTO);
+        return "redirect:/players";
     }
 
 }
